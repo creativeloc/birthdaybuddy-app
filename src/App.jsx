729e4b5e8fde5ecd1d8ded33906b7ca054c37dce
creativeloc <creativeloc@yahoo.com>
2025-05-06 01:27:55 -0400

    clea all functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,18 @@ import List from "./components/List"
 const App = () => {
   const [people, setPeople] = useState(data)
 
+  const handleClick = () => {
+    setPeople([])
+  }
+
   return (
     <main>
       <section className="container">
         <h3>{people.length} birthdays today</h3>
         <List people={people} />
+        <button type="button" className="btn btn-block" onClick={handleClick}>
+          Clear All
+        </button>
       </section>
     </main>
   )
